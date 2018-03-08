b9b616ade935ca585b5d7ab985b74478499fb3ba
Kareem Omar <kareemomar96@hotmail.com>
2018-03-08 14:46:45 +0200

    sign up and sign in mod. ver 1.1

--- easyc/app/src/main/java/com/example/android/easyc/sign_in_activity.java
+++ easyc/app/src/main/java/com/example/android/easyc/sign_in_activity.java
@@ -1,7 +1,9 @@
 package com.example.android.easyc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class sign_in_activity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class sign_in_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_activity);
+    }
+
+
+
+
+
+
+    public void open_sign_up(View view)
+    {
+        Intent i = new Intent(this,sign_up.class);
+        startActivity(i);
     }
 }

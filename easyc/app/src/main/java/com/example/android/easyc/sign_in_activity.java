4d0500b52f517010b9b07f42240daf970188002e
medo talaat <mohamedtalaat0111790@gmail.com>
2018-03-09 18:05:07 +0200

    student activity

--- easyc/app/src/main/java/com/example/android/easyc/sign_in_activity.java
+++ easyc/app/src/main/java/com/example/android/easyc/sign_in_activity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.sql.ResultSet;
@@ -24,6 +25,14 @@ public class sign_in_activity extends AppCompatActivity {
 
 
 
+
+
+
+    public void open_student_menu(View view)
+    {
+        Intent i = new Intent(this,student_menu.class);
+        startActivity(i);
+    }
 
 
     public void open_sign_up(View view)
--- easyc/app/src/main/java/com/example/android/easyc/student_menu.java
+++ easyc/app/src/main/java/com/example/android/easyc/student_menu.java
@@ -1,7 +1,13 @@
 package com.example.android.easyc;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class student_menu extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class student_menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_menu);
-    }
+
+      ImageView view = (ImageView) findViewById(R.id.go_to_categories);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(student_menu.this,categories.class);
+                startActivity(i);
+
+            }});
+
+
+}
 }

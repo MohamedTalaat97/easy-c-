52aec8fead819f24307e152f5613278d44e2ede8
medo talaat <mohamedtalaat0111790@gmail.com>
2018-03-15 16:43:31 +0200

    l

--- easyc/app/src/main/java/com/example/android/easyc/Views/categories.java
+++ easyc/app/src/main/java/com/example/android/easyc/Views/categories.java
@@ -2,6 +2,7 @@ package com.example.android.easyc.Views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import com.example.android.easyc.R;
 
@@ -11,5 +12,14 @@ public class categories extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,c);
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.);
     }
 }
